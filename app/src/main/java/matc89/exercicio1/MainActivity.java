5dc9ba4e2685196f2ac5979b612a3f63f1c6fe0e
Pedro Pontes <pontescpedro@gmail.com>
2019-08-23 00:15:42 -0300

    Add text field and button
    Also implemented methods to save and load the app state and another method
    for the button to copy the text from the text field and paste on the
    label

--- app/src/main/java/matc89/exercicio1/MainActivity.java
+++ app/src/main/java/matc89/exercicio1/MainActivity.java
@@ -2,12 +2,51 @@ package matc89.exercicio1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView message;
+    private Button btn;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        message = (TextView)findViewById(R.id.labelMensagem);
+        btn = (Button)findViewById(R.id.btnCumprimentar);
+        editText = (EditText)findViewById(R.id.editNome);
+    }
+
+    public void cumprimentar(View v) {
+        String text = editText.getText().toString();
+
+        text = "Alô, " + text + "!";
+
+        message.setText(text);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString("editText", editText.getText().toString());
+        outState.putString("message", message.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        String text = savedInstanceState.getString("editText");
+        editText.setText(text);
+
+        text = savedInstanceState.getString("message");
+        message.setText(text);
     }
 }
